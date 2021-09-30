929091de9156c5676f9ff104c831d92bca1a3b70
dominic@authorityfile.co.uk <dominic@authorityfile.co.uk@4bbacb08-7324-11e7-851f-d74be21045cd>
2021-09-30 14:02:19 +0000

    hack at symbols script to work with gcc
    git-svn-id: https://helixteamhub.cloud/authorityfile/projects/6809board/repositories/subversion/svn-ramdac/trunk68k@550 4bbacb08-7324-11e7-851f-d74be21045cd

--- scripts/getsymbols_gnu.pl
+++ scripts/getsymbols_gnu.pl
@@ -1,5 +1,7 @@
 #!/bin/perl
 
+#NOTE: if removes directory information from .o/.s filenames and may get
+#confused if same symbols in multiple files with same name!
 
 sub Usage($$$) {
 	my ($fh, $message, $die) = @_;
@@ -33,38 +35,82 @@ my %areas = ();
 while (<$fh_m>) {
 	my $l = $_;
 	$l =~ s/[\r\n\s]*$//;
-	if ($l =~ /s*(\.\w+)\s*(0x[0-9A-F]+)\s*0x[0-9A-F]+\s*(.+?)(\s|$)/i) {
+	if ($l =~ /s*(\.(?:\.|\w)+)\s*(0x[0-9A-F]+)\s*0x[0-9A-F]+\s*(.+?)(\s|$)/i) {
 		my ($name, $base, $file) = ($1, $2, $3);
-		$file =~ s/.*?\/([^\/]+)\.o$/$1/;
 		$areas{"$file:$name"} = $base;
+	} elsif ($l =~ /^\s*(0x[0-9A-F]+)\s*(\w+?)\s*$/i) {
+		my ($base, $symbol) = ($1, $2);
+		$areas{"*COM*:$symbol"} = $base;
 	}
 }
 
+
+print STDERR "=====\nAREAS\n=====\n";
+for my $k (sort keys %areas) {
+	printf STDERR "%08X <= %s\n", $areas{$k}, $k;
+}
+
+print STDERR "\n\n";
+
 close $fh_m;
+
+my $input_file="";
+my $output_file="";
 
 while (my $fn = shift) {
 	open (my $fh_s, "<", $fn) or die "Cannot open \"$fn\" : $!";
 
 	my $sym = 0;
 	while (<$fh_s>) {
-		if ($_ =~ /^DEFINED SYMBOLS/) {
+		my $l = $_;
+		$l =~ s/[\s\r\n]+$//;
+
+		if ($l =~ /^DEFINED SYMBOLS/) {
 			last;
-		}
+		} elsif ($l =~ /^\s*input file\s*:\s*(.+?)\s*$/) {			
+			$input_file = $1;
+			print STDERR "===INPUT $input_file\n";
+		} elsif ($l =~ /^\s*output file\s*:\s*(.+?)\s*$/) {
+			$output_file = $1;
+			print STDERR "===OUTPUT $output_file\n";
+		} 
+
 	}
 
 	while (<$fh_s>) {
-		if ($_ =~ /^\s+([^:]+):\d+\s+(\.\w+):([0-9A-F]+)\s(\w+)/i) {
+		my $l = $_;
+		$l =~ s/[\s\r\n]+$//;
+		if ($l =~ /^\s+([^:]+):\d+\s+(\.(?:\.|\w)+):([0-9A-F]+)\s(\w+)/i) {
 
 			my ($file, $area, $offs, $sym) = ($1, $2, $3, $4);
-			$file =~ s/^(.*?)(.s|.asm)\s*$/\1/i;
 
-			my $area = $areas{"$file:$area"};
+			if ($input_file && $output_file) {
+				$file =~ s/\Q$input_file/$output_file/;
+			}
 
-			my $addr = (hex($area) + hex($offs)) & 0xFFFFFFFF;
+			my $base = $areas{"$file:$area"};
+
+			if (!$base) {
+				die "Cannot find area $file:$area"
+			}
+
+			my $addr = (hex($base) + hex($offs)) & 0xFFFFFFFF;
 
 			printf "DEF %s 0x%08X\n", $sym, $addr;
-		} elsif ($_ =~ /^(\s+([^:]+):\d+)?\s+\*ABS\*:([0-9A-F]+)\s(\w+)/i) {
+		} elsif ($l =~ /^(\s+([^:]+):\d+)?\s+\*ABS\*:([0-9A-F]+)\s(\w+)/i) {
 			my ($sym, $addr) = ($4, $3);
+			$addr = hex($addr) & 0xFFFFFFFF;
+			printf "DEF %s 0x%08X\n", $sym, $addr;
+		} elsif ($l =~ /^(\s+([^:]+):\d+)?\s+\*COM\*:([0-9A-F]+)\s(\w+)/i) {
+			my ($sym) = ($4);
+
+			my $addr = $areas{"*COM*:$sym"};
+
+			if (!$addr) {
+				die "Cannot find common symbol $sym";
+			}
+
+
 			$addr = hex($addr) & 0xFFFFFFFF;
 			printf "DEF %s 0x%08X\n", $sym, $addr;
 		}
